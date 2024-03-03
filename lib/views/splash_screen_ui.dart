845c497f39d7d91f88cce61e464cb72a5d989d30
Ratthee Jarathbenjawong <dev@dnratthee.me>
2024-03-03 21:25:59 +0700

    Create view UI - Splash screen - Login page - Register page

---
+++ lib/views/splash_screen_ui.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'login_ui.dart';
+
+class SplashScreenUI extends StatefulWidget {
+  const SplashScreenUI({super.key});
+
+  @override
+  State<SplashScreenUI> createState() => _SplashScreenUIState();
+}
+
+class _SplashScreenUIState extends State<SplashScreenUI> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return const LoginUI();
+      }));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image(
+              image: const AssetImage('assets/images/logo.png'),
+              width: MediaQuery.of(context).size.width * 0.75),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+          const CircularProgressIndicator(),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+          Text(
+            'IoT SAU 2024',
+            style: GoogleFonts.kanit(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ],
+      )),
+    );
+  }
+}

d0e1824627d81e1bf32292c7513cb50ef0b2e1e0
Ratthee Jarathbenjawong <dev@dnratthee.me>
2024-03-30 20:16:11 +0700

    Add Page A/C 1 to 3 with dynamic class InfoAirConUI

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Config {
-  static const String BASE_URL = 'https://deviot.dnratthee.me/api';
+  static const String BASE_URL = 'deviot.dnratthee.me';
   static const Color COLOR_RED = Color(0xff9c1a25);
   static const Color COLOR_BLUE = Color(0xff020367);
 }

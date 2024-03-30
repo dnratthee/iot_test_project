a4ff6864c2414fe77c5fa40fe94663cea1b17d17
Ratthee Jarathbenjawong <dev@dnratthee.me>
2024-03-30 10:41:47 +0700

    update readme

--- lib/views/info_air_conditioner.dart
+++ lib/views/info_air_conditioner.dart
@@ -2,11 +2,7 @@ import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:iot_test_project/models/room.dart';
 import 'package:iot_test_project/services/call_api.dart';
-import 'package:iot_test_project/utils/config.dart';
-import 'package:iot_test_project/utils/store.dart';
-import 'package:iot_test_project/views/login_ui.dart';
 import 'package:iot_test_project/widgets/my_appbar.dart';
-import 'package:iot_test_project/widgets/my_button_menu.dart';
 import 'package:iot_test_project/widgets/my_padding.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
